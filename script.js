6aa1251a233fc0b96a289feeb60fc3bac8952dde
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-05-21 20:17:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-function getFormvalue() {
-    // Get the form element by its ID
-    var form = document.getElementById('form1');
+// function getFormvalue() {
+//     // Get the form element by its ID
+//     var form = document.getElementById('form1');
     
-    // Retrieve the values of the first and last name input fields
-    var firstName = form.elements['fname'].value;
-    var lastName = form.elements['lname'].value;
+//     // Retrieve the values of the first and last name input fields
+//     var firstName = form.elements['fname'].value;
+//     var lastName = form.elements['lname'].value;
     
-    // Combine the first and last name into a single string
-    var fullName = firstName + ' ' + lastName;
+//     // Combine the first and last name into a single string
+//     var fullName = firstName + ' ' + lastName;
     
-    // Display the full name using alert
-    alert(fullName);
+//     // Display the full name using alert
+//     alert(fullName);
+// }
+function getFormvalue(){
+	var form=document.getElementById("form1")
+
+	var firstName=form.element("fname").value;
+	var=lastName=form.element("lname").value;
+
+	var fullName=firstName+" "+lastName;
+
+	alert(fullName);
 }
